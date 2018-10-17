adef34144e2f5168612952b7c55f14acdd604d86
coderming <coderming@foxmail.com>
2018-10-17 08:55:53 +0800

    ci: 添加部分单元测试

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ export function PubSub() {
 }
 
 PubSub.prototype.subscribe = function (channel, callback) {
-    this.ec.addEventListener(channel, callback);
+    this.ec.addListener(channel, callback);
 
-    return {channel, callback}
-}
+    return { channel, callback };
+};
 
 PubSub.prototype.unsubscribe = function (channel, callback) {
     if (isObject(channel) && !isFunction(callback)) {
@@ -24,11 +24,11 @@ PubSub.prototype.unsubscribe = function (channel, callback) {
     } else {
         this.ec.removeAllListeners(channel);
     }
-}
+};
 
 PubSub.prototype.publish = function (channel, ...args) {
-    this.ec.emit(channel,  ...args);
-}
+    this.ec.emit(channel, ...args);
+};
 
 PubSub.prototype.sub = PubSub.prototype.subscribe;
 PubSub.prototype.unsub = PubSub.prototype.unsubscribe;
@@ -36,17 +36,17 @@ PubSub.prototype.pub = PubSub.prototype.publish;
 
 const pb = new PubSub();
 
-export const subscribe = function(channel, callback) {
+export const subscribe = function (channel, callback) {
     return pb.subscribe(channel, callback);
-}
+};
 
-export const unsubscribe = function(channel, callback) {
+export const unsubscribe = function (channel, callback) {
     return pb.unsubscribe(channel, callback);
-}
+};
 
-export const publish = function(channel, ...args) {
+export const publish = function (channel, ...args) {
     return pb.publish(channel, ...args);
-}
+};
 
 export const sub = subscribe;
 
--- test/test.js
+++ test/test.js
@@ -1,19 +1,20 @@
-var expect = require('expect.js');
+var expect = require('expect.js')
 
-var base = require('../dist/index.js');
+var base = require('../dist/index.js')
 
 describe('单元测试', function() {
-    this.timeout(1000);
+    this.timeout(1000)
 
-    describe('功能1', function() {
-        it('相等', function() {
-            expect(base.name).to.equal('base');
-        });
-    });
+    describe('触发订阅', function() {
+        base.sub('hello', function(...args) {
+            it('相等', function() {
+                expect(args[0]).to.equal('test')
+            })
+        })
+        base.pub('hello', 'test')
+    })
 
-    describe('功能2', function() {
-        it('不相等', function() {
-            expect(base.name).not.to.equal(1);
-        });
-    });
-});
+    describe('取消订阅', function() {
+        // ...
+    })
+})
